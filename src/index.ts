dfe7aded8e1f6099024c736243481922b7220c7b
agus022 <flores.jorge.1j@gmail.com>
2025-07-09 18:35:45 -0600

    libraries

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors';
 import server from './server';
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`REST API funcionando desde el puerto: ${PORT}`);
+  console.log(colors.cyan.bold (`REST API funcionando desde el puerto: ${PORT}`));
 });
