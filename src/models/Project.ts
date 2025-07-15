64cd5d5de92f5402e6dc54c46895437fdfa62f92
agus022 <flores.jorge.1j@gmail.com>
2025-07-14 18:17:50 -0600

    modelProject

---
+++ src/models/Project.ts
@@ -0,0 +1,28 @@
+import mongoose, {Schema, Document} from "mongoose";
+//typescript
+export type ProjectType = Document & {
+    projectName: string;
+    clientName: string;
+    description: string;
+}
+//mongoose schema for Project
+const ProjectSchema: Schema = new Schema({  
+    projectName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    clientName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+const Project = mongoose.model<ProjectType>('Project', ProjectSchema);
+export default Project;
